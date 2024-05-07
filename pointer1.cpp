fd0004afff774600ee001629270f1b1695752168
SASCHADDD <saschadanu23@gmail.com>
2024-05-07 18:23:29 +0700

    membuat class siswa

--- pointer1.cpp
+++ pointer1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class mahasiswa{
-    public;
+    public:
     int nim;
     void showNim(){
         cout<<"No Induk = "<<nim<<endl;
@@ -14,4 +14,12 @@ int main (){
     mahasiswa mhs{1}; //object mhs
     mhs.showNim(); //Member Access Operator
 
+    mahasiswa &refMhs = mhs; //pointer Reference refMhs
+    refMhs.nim = 2; //MemberAccess operation
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa *pMhs = &mhs; //pointer dereference pMhs
+    pMhs->nim = 3; //Arrow operator
+    pMhs->showNim();
+    return 0;
 }
