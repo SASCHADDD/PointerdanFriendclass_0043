bca4c43d3f52528be4c55efb9eeedbb431a774bd
SASCHADDD <saschadanu23@gmail.com>
2024-05-07 18:53:55 +0700

    friend functionv  2

---
+++ friendfunctiondua.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+    public:
+        void showNimPelajar(pelajar &x);
+};
+
+class pelajar
+{
+    private:
+        int nilai;
+
+    public:
+        pelajar() {nilai = 100; }
+        friend void manusia::showNimPelajar(pelajar &x);
+};
+
+void manusia::showNimPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNimPelajar(pbudi);
+    return 0;
+}
